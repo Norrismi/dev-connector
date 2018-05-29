e50f059bab1618f3accf48886adb368d3a4c2f98
Norrismi <Norrismi0@gmail.com>
2018-05-29 14:49:18 -0400

    user almost complete

---
+++ models/User.js
@@ -0,0 +1,30 @@
+//naming for models: keep singular and first letter capitalize
+
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const User = mongoose.model("users", UserSchema);
+
+module.exports = User;
